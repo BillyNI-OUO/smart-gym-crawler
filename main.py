3fb1dbc4f57f8242bdd8bf1807fd9f55ec0607c9
howard <howard89213@gmail.com>
2021-04-30 23:15:53 +0800

    增加功能 計時功能與增大座標範圍

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ import src.crawler as crawler
 
 #import src.sql as sql
 from src.sql.connector import connector
+import time 
 
 
 con = connector()
 
 con.init_db()
-
-l = crawler.grid.search_nearby2((22.0, 22.0002), (120.7440363, 120.7440563))
+run_start=time.time()
+l = crawler.grid.search_nearby2((22.0, 25.0002), (120.7440363, 120.7440563))
 for i in l:
 	print(i)
 	con.insert_place(i)
 	ll = crawler.query.reviews((i.cid_1, i.cid_2))
 	con.insert_reviews(ll)
-
+run_end=time.time()
+print("執行時間：%f 秒" %(run_end-run_start))
 
 """
 cor_list = crawler.coordinate.get_coordinate('./coordinates/台中.txt')
